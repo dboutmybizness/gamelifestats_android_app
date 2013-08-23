28b5d2525c8065446c1b219ced87f6a61ef3b131
Darnell Lynch <darnell.lynch@gmail.com>
2013-08-23 12:45:02 -0400

    database working

---
+++ src/com/gamelifestats/glselevate/DBAdapter.java
@@ -0,0 +1,100 @@
+package com.gamelifestats.glselevate;
+
+import java.sql.SQLException;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+public class DBAdapter {
+	
+	static final String KEY_ROWID = "_id";
+	static final String KEY_NAME = "name";
+	static final String KEY_POSITION = "position";
+	static final String KEY_HAS_SCOUT = "has_scout";
+	static final String TAG = "DBAdapter";
+	
+	static final String DATABASE_NAME = "GLSDB";
+	static final String DATABASE_TABLE = "scout";
+	static final int DATABASE_VERSION = 1;
+	
+	static final String DATABASE_CREATE = 
+			"create table scout ("+
+			"_id integer primary key autoincrement, "+
+			"name text not null, "+
+			"position text not null, "+
+			"has_scout integer" +
+			")";
+	
+	final Context context;
+	
+	DatabaseHelper DBHelper;
+	SQLiteDatabase db;
+	
+	public DBAdapter(Context ctx){
+		this.context = ctx;
+		DBHelper = new DatabaseHelper(context);
+	}
+	
+	
+	
+	private static class DatabaseHelper extends SQLiteOpenHelper{
+		DatabaseHelper(Context context){
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+		
+		@Override
+		public void onCreate(SQLiteDatabase db){
+			db.execSQL(DATABASE_CREATE);
+			createInitial(db);
+			
+		}
+		
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+			Log.w(TAG, "Upgrading database from version "+ oldVersion + " to " + newVersion + ", which will destroy old data");
+			db.execSQL("DROP TABLE IF EXISTS scout");
+			onCreate(db);
+		}
+	}
+	
+	public DBAdapter open() throws SQLException{
+		db = DBHelper.getWritableDatabase();
+		return this;
+	}
+	
+	public void close(){
+		DBHelper.close();
+	}
+	
+	private static void createInitial(SQLiteDatabase db){
+		ContentValues initialValues = new ContentValues();
+		initialValues.put(KEY_HAS_SCOUT, 0);
+		initialValues.put(KEY_ROWID, 1);
+		initialValues.put(KEY_NAME, "my name");
+		initialValues.put(KEY_POSITION, "Guard");
+		db.insert(DATABASE_TABLE, null, initialValues);
+	}
+	
+	//---below is public quering---
+	public boolean updateScout(String name, String position){
+		ContentValues args = new ContentValues();
+		args.put(KEY_NAME, name);
+		args.put(KEY_POSITION, position);
+		args.put(KEY_HAS_SCOUT, 1);
+		return db.update(DATABASE_TABLE, args, KEY_ROWID+"="+1, null) > 0;
+	}
+	public Cursor getScout() throws SQLException{
+		Cursor mCursor = 
+				db.query(true, DATABASE_TABLE, new String[] {KEY_ROWID, KEY_NAME, KEY_POSITION}, KEY_ROWID +"="+1, null, null, null, null, null);
+		if ( mCursor != null){
+			mCursor.moveToFirst();
+		}
+		return mCursor;
+	}
+	
+}
--- src/com/gamelifestats/glselevate/GLS_Act_ScoutEdit.java
+++ src/com/gamelifestats/glselevate/GLS_Act_ScoutEdit.java
@@ -1,6 +1,8 @@
 package com.gamelifestats.glselevate;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
@@ -8,9 +10,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class GLS_Act_ScoutEdit extends Activity {
 
+	Cursor c;
+	int row = 453;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +28,33 @@ public class GLS_Act_ScoutEdit extends Activity {
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.positions_array, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		positions.setAdapter(adapter);
+		
+		DBAdapter db = new DBAdapter(this);
+		try {
+			db.open();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (java.sql.SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			
+			c = db.getScout();
+			row = c.getInt(0);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (java.sql.SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		db.close();
+		
+		Toast.makeText(this, "ddfs "+row, Toast.LENGTH_SHORT).show();
 
 	}
 
