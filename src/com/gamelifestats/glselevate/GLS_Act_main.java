dc2198a5113271a4469605f055eaddae1c35c5f0
Darnell Lynch <darnell.lynch@gmail.com>
2013-11-02 13:57:09 -0400

    partial

--- src/com/gamelifestats/glselevate/GLS_Act_main.java
+++ src/com/gamelifestats/glselevate/GLS_Act_main.java
@@ -3,24 +3,21 @@ package com.gamelifestats.glselevate;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class GLS_Act_main extends Activity {
-	TextView statline,upload_stats;
+	Button stats,upload_game;
 	MCareer career;
 	
 	TextView points,rebounds,assists,games,steals,blocks,turnovers,fouls,minutes;
-	View elevate_link;
-	TextView gls_link;
+
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +26,7 @@ public class GLS_Act_main extends Activity {
         
         ActionBar actionBar = getActionBar();
         actionBar.setTitle(R.string.home_court);
-        
-        elevate_link = (View) findViewById(R.id.elevate_child);
-        elevate_link.setOnLongClickListener(new OnLongClickListener(){
-			@Override
-			public boolean onLongClick(View v) {
-				goToGLS();
-				return false;
-			}
-        });
-        
-        gls_link = (TextView) findViewById(R.id.gls_com);
-        gls_link.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				goToGLS();
-			}
-        });
+  
         
         points = (TextView) findViewById(R.id.dis_points);
         games = (TextView) findViewById(R.id.dis_games);
@@ -59,16 +40,16 @@ public class GLS_Act_main extends Activity {
         
         career = new MCareer(this);
         
-        statline = (TextView) findViewById(R.id.statline);
-        statline.setOnClickListener(new OnClickListener(){
+        stats = (Button) findViewById(R.id.button_stats);
+        stats.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
 				startActivity(new Intent(getBaseContext(), Stats.class));
 			}
         });
-        upload_stats = (TextView) findViewById(R.id.upload_gamestats);
-        upload_stats.setOnClickListener(new OnClickListener(){
+        upload_game = (Button) findViewById(R.id.button_upload_game);
+        upload_game.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
@@ -117,14 +98,6 @@ public class GLS_Act_main extends Activity {
     		return true;
     	}
     	return super.onOptionsItemSelected(item);
-    }
-    
-    private void goToGLS(){
-    	Resources res = getResources();
-    	String url = res.getString(R.string.gls_site);
-    	Intent i = new Intent(Intent.ACTION_VIEW);
-    	i.setData(Uri.parse(url));
-    	startActivity(i);
     }
     
 }
